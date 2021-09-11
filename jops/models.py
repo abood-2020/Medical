201492d616acd968bac0a22f4e03aa1506834109
Abd Al Razzaq Al Dahdouh <abdrazzaq20182019@gmail.com>
2021-09-11 10:48:41 +0300

    add item Job to model

--- jops/models.py
+++ jops/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+# Django Models are
+#   Validtions and html weight 
+
+JOB_TYPE = (
+    ('Full-Time','Full-Time'),
+    ('Part-Time','Part-Time'),
+)
+
+class Job(models.Model): #table
+    titel       = models.CharField(max_length=100) #colume
+    jop_type    = models.CharField(max_length=20,default='FullTime',choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published   = models.DateTimeField(auto_now = True)
+    Vacany      = models.IntegerField(default=1)
+    salary      = models.IntegerField(default=0)
+    experience  = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.titel
+ 
+
